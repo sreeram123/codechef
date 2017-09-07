948ea2cb8ad934356787b8af6f9208e0c41e14fa
navisk13 <navisk13@gmail.com>
2017-09-07 21:21:29 +0530

    EnormousInput

---
+++ EnormousInput.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class EnormousInput{
+  public static void main(String args[])
+  {
+    long m,div,n;
+    int c=0;
+    Scanner sc=new Scanner(System.in);
+    m=sc.nextLong();
+    div=sc.nextLong();
+    for(int i=0;i<m;i++)
+    {
+      n=sc.nextLong();
+      if(n%div==0)
+      c++;
+    }
+    System.out.println(c);
+  }
+}
